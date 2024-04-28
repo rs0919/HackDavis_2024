0e48f20587e5017b239c2dd871cc728340bed42c
Rohith Saravana <rsaravana@ucdavis.edu>
2024-04-27 20:18:55 -0700

    can upload images to database

--- mysite/encoder/views.py
+++ mysite/encoder/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .forms import ImgForm
 
 # Create your views here.
 
 def index(request):
     return HttpResponse("encode your image here.")
+
+def image_view(request):
+    
+    if request.method == 'POST':
+        form = ImgForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = ImgForm()
+    
+    return render(request, 'image_form.html', {'form': form})
+
+def success(request):
+    return HttpResponse('successfully uploaded')
