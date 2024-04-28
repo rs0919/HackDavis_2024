442bfc6e87eabd95251bf9f1a5c826f7ca8a4422
Rohith Saravana <rsaravana@ucdavis.edu>
2024-04-27 22:54:37 -0700

    added text decoding (for only one image)

--- mysite/decoder/views.py
+++ mysite/decoder/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from .forms import ImgToDecodeForm
 from .models import ImgToDecode
+
+from PIL import Image
 # Create your views here.
 
 def image_view(request):
@@ -23,4 +25,29 @@ def index(request):
         return HttpResponse("placeholder text")
 
 def secret_message_view(request):
-    return HttpResponse("should be secret img or text here")
+    secret_message = decode_img("./media/images_to_decode/encoded_image.png") # change
+    return HttpResponse("secret_message:" + secret_message)
+
+
+def bin_to_message(binary):
+    """Convert binary data to message."""
+    message = []
+    for i in range(0, len(binary), 8):
+        byte = binary[i:i+8]
+        message.append(chr(int(byte, 2)))
+    return ''.join(message)
+
+
+def decode_img(filepath):
+    img = Image.open(filepath)
+    binary_message = ''
+    width, height = img.size
+
+    for row in range(height):
+        for col in range(width):
+            pixel = list(img.getpixel((col, row)))
+            for n in range(3):
+                binary_message += str(pixel[n] & 1)
+                if binary_message[-16:] == '1111111111111110':
+                    return bin_to_message(binary_message[:-16])
+    return "No hidden message found."
