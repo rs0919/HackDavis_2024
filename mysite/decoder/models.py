f753a3ac4ed32febe4c316a6be799a8521c1d13e
Rohith Saravana <rsaravana@ucdavis.edu>
2024-04-27 22:28:02 -0700

    filling out boiler plate code

--- mysite/decoder/models.py
+++ mysite/decoder/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class SecretMessage(models.Model):
+    secret_msg = models.CharField(max_length=100)
+    secret_img = models.ImageField(upload_to='image_to_decode/')
+
+class ImgToDecode(models.Model):
+    img = models.ImageField(upload_to='images/')
--- mysite/decoder/views.py
+++ mysite/decoder/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
+from .forms import ImgToDecodeForm
+from .models import ImgToDecode
 # Create your views here.
+
+def image_view(request):
+    
+    if request.method == 'POST':
+        form = ImgToDecodeForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('secret_message_view')
+    else:
+        form = ImgToDecodeForm()
+    
+    return render(request, 'image_to_decode_form.html', {'form': form})
